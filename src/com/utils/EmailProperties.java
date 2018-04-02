e750fed783c3b51bce59ad7fa8c71681f206c8e2
Jack Lo <36879850+chikit-lo@users.noreply.github.com>
2018-04-02 10:32:20 +0800

    Update EmailProperties.java

--- src/com/utils/EmailProperties.java
+++ src/com/utils/EmailProperties.java
@@ -2,6 +2,6 @@ package com.utils;
 
 public class EmailProperties {
 	public static final String HOSTNAME = "smtp.abckms.com";
-	public static final String EMAILACCOUNT = "jingxingit@abckms.com";
-	public static final String EMAILPASSWORD = "123abc";
+	public static final String EMAILACCOUNT = "XXX@abckms.com";
+	public static final String EMAILPASSWORD = "XXX";
 }
